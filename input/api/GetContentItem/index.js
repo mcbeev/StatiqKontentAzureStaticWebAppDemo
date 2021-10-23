f99563b66a3db23661d0683ad4e7d219a392c63f
Brian McKeiver <bmckeiver@bizstream.com>
2021-10-23 16:44:55 -0400

    Updating API call for Kontent.

--- input/api/GetContentItem/index.js
+++ input/api/GetContentItem/index.js
@@ -3,13 +3,16 @@ const fetch = require("node-fetch");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const codeName = (req.query.codeName || (req.body && req.body.codeName));
+    const codeName = (req.query.codename || (req.body && req.body.codename));
 
-    const url = process.env.KontentDeliveryAPIEndpoint + process.env.KontentProjectID + "/items/"; 
+    const url = process.env.KontentDeliveryAPIEndpoint 
+                    + process.env.KontentProjectID 
+                    + "/items/"
+                    + codeName; 
 
     context.log("url:" + url);
 
-    await fetch(url + codeName)
+    await fetch(url)
         .then(response => response.json())
         .then(response => context.res.json(response.item.elements.content.value)); 
     
--- output/api/GetContentItem/index.js
+++ output/api/GetContentItem/index.js
@@ -3,12 +3,17 @@ const fetch = require("node-fetch");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const codeName = (req.query.codeName || (req.body && req.body.codeName));
+    const codeName = (req.query.codename || (req.body && req.body.codename));
 
-    const url = process.env.KontentDeliveryAPIEndpoint + process.env.KontentProjectID + "/items/"; 
-    
-    await fetch(url + codeName)
+    const url = process.env.KontentDeliveryAPIEndpoint 
+                    + process.env.KontentProjectID 
+                    + "/items/"
+                    + codeName; 
+
+    context.log("url:" + url);
+
+    await fetch(url)
         .then(response => response.json())
-        .then(response => context.res.json(response.item.elements[0].value)); 
+        .then(response => context.res.json(response.item.elements.content.value)); 
     
 }
