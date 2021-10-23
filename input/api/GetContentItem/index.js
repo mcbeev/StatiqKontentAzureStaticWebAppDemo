e4a03e525a2753c977d7a29ac67cd6940b62c337
Brian McKeiver <bmckeiver@bizstream.com>
2021-10-23 16:05:31 -0400

    Adding in GetContentItem API.

---
+++ input/api/GetContentItem/index.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const codeName = (req.query.codeName || (req.body && req.body.codeName));
+
+    const url = process.env.KontentDeliveryAPIEndpoint + process.env.KontentProjectID + "/items/"; 
+    
+    await fetch(url + codeName)
+        .then(response => response.json())
+        .then(response => context.res.json(response.item.elements[0].value)); 
+    
+}
